9f36df56f066ba7375056a9cbe5f798c48673bc6
Michał Niskowski <niski315@gmail.com>
2017-12-12 14:14:32 +0100

    Prepare for for eigen compute and create propability class in C#

---
+++ MushroomsCs/Propability.cs
@@ -0,0 +1,25 @@
+﻿namespace MushroomsCs
+{
+    public class Propability
+    {
+        public double[,] CreateMatrixWithPropability()
+        {
+            int mapLength = 7;
+            bool isQubeEqual = true;
+
+            double qubePropability = 1d / 7d;
+            double[] qubeValues = {-3, -2, -1, 0, 1, 2, 3};
+
+            int playerOnePosition = -3;
+            int playerTwoPosition = 3;
+
+            int i = 0;
+
+            double[,] resMatrix = new double[100, 100];
+            
+
+
+            return null;
+        }
+    }
+}
